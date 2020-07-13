ee918f634f29c7ef492a3abeffcab7279195712c
Evan Young <cantis@gmail.com>
2020-07-13 15:49:21 -0500

    Working Through Setup and DB Refactor

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_fontawesome import FontAwesome
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Email, Length, AnyOf
+
 
 app = app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -13,20 +14,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI'
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
+
 bs = Bootstrap(app)
+
 fa = FontAwesome(app)
+
 db = SQLAlchemy()
 db.init_app(app)
 
 
 class User(db.Model):
+    """ Data model for user account """
     __tablename__ = 'users'
     id = db.Column(
         db.Integer,
         primary_key=True
     )
     username = db.Column(
-        db.String(64),
+        db.String(20),
         index=False,
         unique=True,
         nullable=False
@@ -35,14 +40,23 @@ class User(db.Model):
         db.String(32),
         index=False
     )
+    firstname = db.Column(
+        db.String(20)
+    )
+    lastname = db.Column(
+        db.String(20)
+    )
 
 
 db.create_all()
 
 
 class AddUserForm(FlaskForm):
-    username = StringField(label='Username', validators=[InputRequired(), Email(message="I don\'t like your email.")])
-    password = PasswordField(label='Password', validators=[InputRequired(), Length(min=5, max=10), AnyOf(['secret', 'password'])])
+    """ User Form Fields """
+    email = StringField(label='Email', validators=[InputRequired('An email is required'), Email('Not an email format.')])
+    password = PasswordField(label='Password', validators=[InputRequired('Please enter a password'), Length(min=5, max=10), AnyOf(['secret', 'password'])])
+    firstname = StringField(label='First Name', validators=[InputRequired(message='A first name is required'), Length(min=1, max=20)])
+    lastname = StringField(label='Last Name', validators=[InputRequired('A last name is required'), Length(min=1, max=20)])
 
 
 @app.route('/')
@@ -64,7 +78,9 @@ def show_add_user_form():
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        new_user = User(username=username, password=password)
+        firstname = form.firstname.data
+        lastname = form.lastname.data
+        new_user = User(username=username, password=password, firstname=firstname, lastname=lastname)
         db.session.add(new_user)
         db.session.commit()
         return f'Form Successfully Submitted User:{username} Pass:{password}'
---
+++ models/user.py
@@ -0,0 +1,27 @@
+""" Data Models """
+from app import db
+
+
+class User(db.Model):
+    """ Data model for user account """
+    __tablename__ = 'users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    username = db.Column(
+        db.String(20),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(32),
+        index=False
+    )
+    firstname = db.Column(
+        db.String(20)
+    )
+    lastname = db.Column(
+        db.String(20)
+    )
