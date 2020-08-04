6f731253dae92e881f3a62e22fc6ed8886aad234
Evan Young <cantis@gmail.com>
2020-08-04 13:20:06 -0500

    Create and Update working!

--- app/users/users.py
+++ app/users/users.py
@@ -1,8 +1,7 @@
-from typing import Iterable
 from flask import Blueprint, render_template, redirect, url_for
-from flask_table import Table, Col
+from flask.globals import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, HiddenField
 from wtforms.validators import InputRequired, Email, Length, AnyOf
 
 from app.models.user import User, db
@@ -11,13 +10,22 @@ from app.models.user import User, db
 user_bp = Blueprint('user_bp', __name__, template_folder='templates', static_folder='static')
 
 
-# Form Definition
+# Form Definitions
 class AddUserForm(FlaskForm):
-    """Add User Form Declaration"""
+    """Add User Form"""
     email = StringField(label='Email', validators=[InputRequired('An email is required'), Email('Not an email format.')])
-    password = PasswordField(label='Password', validators=[InputRequired('Please enter a password'), Length(min=5, max=10), AnyOf(['secret', 'password'])])
     firstname = StringField(label='First Name', validators=[InputRequired(message='A first name is required'), Length(min=1, max=20)])
     lastname = StringField(label='Last Name', validators=[InputRequired('A last name is required'), Length(min=1, max=20)])
+    password = PasswordField(label='Password', validators=[InputRequired('Please enter a password'), Length(min=5, max=10), AnyOf(['secret', 'password'])])
+
+
+class EditUserForm(FlaskForm):
+    """Edit User Form"""
+    id = HiddenField()
+    email = StringField(label='Email', validators=[InputRequired('An email is required'), Email('Not an email format.')])
+    firstname = StringField(label='First Name', validators=[InputRequired(message='A first name is required'), Length(min=1, max=20)])
+    lastname = StringField(label='Last Name', validators=[InputRequired('A last name is required'), Length(min=1, max=20)])
+    password = PasswordField(label='Password', validators=[InputRequired('Please enter a password'), Length(min=5, max=10), AnyOf(['secret', 'password'])])
 
 
 # Handlers
@@ -35,15 +43,36 @@ def show_add_user_form():
         new_user = User(email=email, password=password, firstname=firstname, lastname=lastname)
         db.session.add(new_user)
         db.session.commit()
-        # return f'Form Successfully Submitted User:{email} Pass:{password}'
+        
         return redirect(url_for('user_bp.show_user_list_form'))
 
     # if it's just a GET show the form
     return render_template('user_add.html', form=form)
 
 
+@user_bp.route('/user/<id>', methods=['GET', 'POST'])
+def show_edit_user_form(id):
+    """Show user edit form and handle user updates."""
+
+    edit_user = User.query.filter_by(id=id).first()
+    # TODO: deal with not getting a user back from the query
+
+    form = EditUserForm()
+
+    if form.validate_on_submit():
+        edit_user.email = form.email.data
+        edit_user.password = form.password.data
+        edit_user.firstname = form.firstname.data
+        edit_user.lastname = form.lastname.data
+        db.session.commit()
+        return redirect(url_for('user_bp.show_user_list_form'))
+    else:
+        form.process(obj=edit_user)  # this provides the existing data to display on the form
+        return render_template('user_edit.html', form=form, user=edit_user)
+
+
 @user_bp.route('/user', methods=['GET'])
 def show_user_list_form():
     """Show list of current users."""
     userList = User.query.all()
-    return render_template('user_list.html', objects=userList)
+    return render_template('user_list.html', users=userList)
