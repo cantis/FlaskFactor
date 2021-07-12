bde0c4c68654310b4ef43d7a30a873936fc84ffe
Evan <cantis@gmail.com>
2021-07-11 23:21:16 -0500

    List works, getting selection back!

--- web/routes/home.py
+++ web/routes/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
 from web.utility.setting import save_setting
-
+from web.models import Party
 
 # Blueprint Configuration
 home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_folder='static')
@@ -11,7 +11,9 @@ home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_fol
 @login_required
 def index():
     ''' Homepage '''
-    return render_template('/home.html', current_user=current_user, party_menu=True)
+    selected_party = 'Test Party'
+    party_list = Party.query.all()
+    return render_template('/home.html', current_user=current_user, party_menu=True, selected_party=selected_party, party_list=party_list)
 
 
 @home_bp.route('/nav_select_party/<id>', methods=['GET'])
