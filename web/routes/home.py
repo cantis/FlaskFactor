b421dcb547845c43c00eee7c3cd15f759b196979
Evan <cantis@gmail.com>
2021-07-11 23:28:56 -0500

    Party Select Working!

--- web/routes/home.py
+++ web/routes/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
-from web.utility.setting import save_setting
+from web.utility.setting import get_setting, save_setting
 from web.models import Party
 
 # Blueprint Configuration
@@ -11,7 +11,12 @@ home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_fol
 @login_required
 def index():
     ''' Homepage '''
-    selected_party = 'Test Party'
+    selected_id = get_setting('current_party')
+
+    if selected_id:
+        selected_party = Party.query.get(selected_id).party_name
+    else:
+        selected_party = 'Please Select'
     party_list = Party.query.all()
     return render_template('/home.html', current_user=current_user, party_menu=True, selected_party=selected_party, party_list=party_list)
 
