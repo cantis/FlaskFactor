b5749a565e78ec7e608348b9f830aa6b04403734
Evan Young <cantis@gmail.com>
2022-01-08 23:07:57 -0600

    Fixed up test running problem

--- web/routes/receiving.py
+++ web/routes/receiving.py
@@ -19,8 +19,8 @@ receiving_bp = Blueprint('receiving_bp', __name__, template_folder='templates', 
 
 # Form Definitions
 class AddItemForm(FlaskForm):
-    ''' Form for receiving items '''
-    name = StringField(label='Name', validators=[DataRequired()])
+    ''' Form for adding receiving items '''
+    item_name = StringField(label='Name', validators=[DataRequired()])
     # type = SelectField('Type', choices=ItemTypeEnum.choices(), validators=[DataRequired()])
     type = StringField(label='Type', validators=[DataRequired()])
     quantity = IntegerField(label='Count', validators=[DataRequired(), NumberRange(min=1)])
@@ -30,9 +30,9 @@ class AddItemForm(FlaskForm):
 
 
 class EditItemForm(FlaskForm):
-    ''' Form for editing items '''
+    ''' Form for editing receiving items '''
     id = HiddenField()
-    name = StringField('Name', validators=[DataRequired()])
+    item_name = StringField('Name', validators=[DataRequired()])
     # type = SelectField('Type', choices=ItemTypeEnum.choices(), validators=[DataRequired()])
     type = StringField('Type', validators=[DataRequired()])
     quantity = IntegerField('Count', validators=[DataRequired(), NumberRange(min=1)])
@@ -42,16 +42,14 @@ class EditItemForm(FlaskForm):
 
 
 # Handlers
-@receiving_bp.route('/receiving', methods=['GET', 'POST'])
+@receiving_bp.route('/receiving', methods=['GET'])
 @login_required
 def show_receiving_list_form():
-    ''' show form for items in a receipt '''
+    ''' show form for items in a receipt, set to add mode to receive items '''
     mode = 'add'
     form = AddItemForm()
 
-    # Get current party selection and dropdown listll
-
-    # Party List
+    # Party list and current party selection
     party_list = Party.query.all()
     selected_party_id = get_common_setting(setting_name='current_party')
     if selected_party_id:
@@ -59,12 +57,38 @@ def show_receiving_list_form():
     else:
         selected_party = 'Please Select'  # TODO: if no party selected, show dialog prompting to select one
 
-    # Get current receiving list
+    # Receiving list data
     received = Receiving.query.filter_by(party_id=selected_party_id, ).all()
 
     # Show the form
     return render_template('receiving.html', mode=mode, current_user=current_user, party_menu=True,
                            selected_party=selected_party, party_list=party_list, received=received, form=form)
+
+
+@receiving_bp.route('/receiving/add', methods=['POST'])
+@login_required
+def add_receiving_item():
+    ''' add receiving item to database '''
+    form = AddItemForm()
+    if form.validate_on_submit():
+        # Get party id
+        party_id = get_common_setting(setting_name='current_party')
+
+        # Create new receiving item
+        new_receiving = Receiving(id=next_receiving_id(party_id), party_id=party_id, name=form.name.data,
+                                  type=form.type.data, quantity=form.quantity.data, value=form.value.data,
+                                  salevalue=form.saleValue.data)
+
+        # Add to database
+        db.session.add(new_receiving)
+        db.session.commit()
+
+    # Redirect to receiving list
+    return redirect(url_for('receiving_bp.show_receiving_list_form'))
+
+    # Show the form again
+    # return render_template('receiving.html', mode='add',
+    #                        current_user=current_user, party_menu=True, form=form)
 
 
 # Utility
