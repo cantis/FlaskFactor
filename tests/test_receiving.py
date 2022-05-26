19fd84b6f4db1bd958a9afb24178607b5673f856
Evan Young <cantis@gmail.com>
2022-05-26 16:50:45 -0500

    Debugging Tests for Receiving

--- tests/test_receiving.py
+++ tests/test_receiving.py
@@ -7,6 +7,7 @@ from web.models import Party, Receiving, Item_Type
 from web.routes.receiving import next_receiving_id
 
 
+# Test Fixtures
 @pytest.fixture(scope='session')
 def app():
     app = create_app()
@@ -34,8 +35,8 @@ def client(app):
         db.drop_all()
 
 
-# Route Tests
-def test_navigate_to_receiving(client):
+# Base Route Tests
+def test_navigate_to_receiving_list(client):
     # arrange
 
     with client.application.test_request_context():
@@ -44,7 +45,85 @@ def test_navigate_to_receiving(client):
 
         # assert
         assert response.status_code == 200
+        assert b'Receiving - Item List' in response.data
 
+
+def test_navigate_to_receiving_add(client):
+    # arrange
+
+    with client.application.test_request_context():
+        # act
+        response = client.get('/receiving/add')
+
+        # assert
+        assert response.status_code == 200
+        assert b'Receiving - Add Item' in response.data
+
+
+def test_navigate_to_receiving_edit(client):
+    # arrange
+    db.session.add(Receiving(
+            receipt_id=1,
+            session_id=1,
+            party_id=1,
+            Item_Type_id='1',
+            quantity=4,
+            isCommitted=False,
+            item='Short Sword',
+            purchase_price=10.00
+        ))
+    db.session.commit()
+
+    with client.application.test_request_context():
+        # act
+        response = client.get('/receiving/edit/1')
+
+        # assert
+        assert response.status_code == 200
+        assert b'Receiving - Edit Item' in response.data
+
+
+# Add Item Tests
+def test_add_receiving_item_valid_data_ok(client):
+    # arrange
+    with client.application.test_request_context():
+        # act
+        response = client.post('/receiving/add', data={
+            'session': 1,
+            'item_name': 'Short Sword',
+            'type': 'Melee',
+            'quantity': 4,
+            'value': 10.00,
+            'saleValue': 10.00,
+            'add_another': True
+        }
+        )
+        # assert
+        assert response.status_code == 200
+
+
+def test_add_receiving_item_invalid_data_fails(client):
+    pass
+
+
+def test_add_receiving_item_with_no_party_fails(client):
+    # arrange
+
+    with client.application.test_request_context():
+        # act
+        response = client.post('/receiving/add', data={
+            'session': 1,
+            'item_name': 'Short Sword',
+            'type': 'Melee',
+            'quantity': 4,
+            'value': 10.00,
+            'saleValue': 10.00,
+            'add_another': True
+        }
+        )
+        # assert
+        assert response.status_code == 200
+        assert b'Please select a party' in response.data
 
 # Utility Tests
 def test_next_receiving_number_returns_one_when_empty(client):
